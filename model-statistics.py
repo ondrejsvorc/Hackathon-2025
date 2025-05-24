dbcb71cb09c9cc4dd0c9053f4cd551335266d1dd
Ondřej Švorc <ondrejsvorc@email.cz>
2025-05-24 13:57:37 +0200

    Fix model statistics

--- model-statistics.py
+++ model-statistics.py
@@ -4,9 +4,11 @@ import os
 import numpy as np
 from lib.loader import FolderExtractor
 
-SIGNAL_NAME = "art"
+SIGNAL_NAME_ART = "art"
+SIGNAL_NAME_ICP = "icp"
 GENERATED_DIR = "generated"
-OUTPUT_FILE = "model-statistics.json"
+OUTPUT_FILE_ART = "model-statistics-art.json"
+OUTPUT_FILE_ICP = "model-statistics-icp.json"
 
 if not os.path.isdir(GENERATED_DIR):
     os.makedirs(GENERATED_DIR)
@@ -32,15 +34,27 @@ def compute_stats(data, file_name):
     )
 
 extractor = FolderExtractor(GENERATED_DIR)
-raw_data_dict = extractor.get_raw_data(SIGNAL_NAME)
+raw_data_dict_art = extractor.get_raw_data(SIGNAL_NAME_ART)
+raw_data_dict_icp = extractor.get_raw_data(SIGNAL_NAME_ICP)
 
 stats_list = []
-for file_stem, signal in raw_data_dict.items():
+for file_stem, signal in raw_data_dict_art.items():
     print(f"Zpracovávám {file_stem}...")
     stats = compute_stats(signal, file_stem)
     stats_list.append(asdict(stats))
 
-with open(OUTPUT_FILE, 'w', encoding='utf-8') as f:
+with open(OUTPUT_FILE_ART, 'w', encoding='utf-8') as f:
     json.dump(stats_list, f, ensure_ascii=False, indent=2)
 
-print(f"\nVýsledky uloženy do {OUTPUT_FILE}")
+print(f"\nVýsledky uloženy do {OUTPUT_FILE_ART}")
+
+stats_list = []
+for file_stem, signal in raw_data_dict_icp.items():
+    print(f"Zpracovávám {file_stem}...")
+    stats = compute_stats(signal, file_stem)
+    stats_list.append(asdict(stats))
+
+with open(OUTPUT_FILE_ICP, 'w', encoding='utf-8') as f:
+    json.dump(stats_list, f, ensure_ascii=False, indent=2)
+
+print(f"\nVýsledky uloženy do {OUTPUT_FILE_ICP}")
